82d7eb9d6b1624ea2e6192f2a081472f7364c630
example1 <32572574+bestshine@users.noreply.github.com>
2021-01-07 23:26:50 +0530

    initial commit
    initial commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'App\Events\SendCommand' => [
+            'App\Listeners\SendCommandListener',
+        ],
+        'App\Events\LocationCreated' => [
+          'App\Listeners\LocationCreatedListener'
+        ]
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
